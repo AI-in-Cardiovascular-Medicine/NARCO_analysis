48d9f2c5deffde1e107aae3eca3b941a8628e2b3
Anselm Stark <anselm.stark@hotmail.com>
2024-02-10 17:24:00 +0100

    cleaned up statistical analysis

--- statistical_analysis/demographics.r
+++ statistical_analysis/demographics.r
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(readxl)
 library(yaml)
-
+library(writexl)
 library(DescTools)
 library(infer)
 
@@ -12,31 +12,6 @@ yaml <- yaml.load_file(file.path(getwd(), "config.yaml")) # only works after act
 baseline <- read_excel(paste0(yaml$first_analysis$output_dir, "/baseline.xlsx"))
 # remove the first row, which is test data
 baseline <- baseline[-1, ]
-
-patterns <- list(
-  hx = "^hx_",
-  sports = "^sports_",
-  ccta = "^ccta_",
-  inv = "^inv_",
-  caa = "^caa_"
-)
-
-filtered_columns <- list()
-for (pattern_name in names(patterns)) {
-  filtered_columns[[pattern_name]] <- grep(patterns[[pattern_name]], names(baseline), value = TRUE)
-  if (pattern_name == "ccta") {
-    filtered_columns[[pattern_name]] <- filtered_columns[[pattern_name]][!grepl("_postop", filtered_columns[[pattern_name]])]
-  }
-}
-
-baseline <- baseline %>% select(all_of(yaml$demographics$dict_variables), all_of(unlist(filtered_columns, use.names = FALSE)))
-
-for (i in seq_along(yaml$demographics$dict_rename)) {
-    new_col_name <- names(yaml$demographics$dict_rename)[i]
-    old_col_name <- yaml$demographics$dict_rename[[i]]
-
-    baseline <- baseline %>% rename(!!new_col_name := !!old_col_name)
-}
 
 ## MUTATE ##########################################################################################################
 baseline <- baseline %>% 
@@ -66,7 +41,9 @@ baseline$hx_sym_ap_ccs <- factor(baseline$hx_sym_ap_ccs, levels = 1:4, ordered =
 baseline$hx_sym_dysp_nyha <- factor(baseline$hx_sym_dysp_nyha, levels = 1:4, ordered = TRUE, labels = c("I", "II", "III", "IV"))
 baseline$sports_level <- factor(baseline$sports_level, levels = 1:3, ordered = TRUE, labels = c("recreational", "competitive", "elite"))
 baseline$sports_static_component <- factor(baseline$sports_static_component, levels = 0:2, ordered = TRUE, labels = c("I", "II", "III"))
+baseline$sports_static_component_ch <- factor(baseline$sports_static_component_ch, levels = 0:2, ordered = TRUE, labels = c("I", "II", "III"))
 baseline$sports_dynamic_component <- factor(baseline$sports_dynamic_component, levels = 0:2, ordered = TRUE, labels = c("A", "B", "C"))
+baseline$sports_dynamic_comp_ch <- factor(baseline$sports_dynamic_comp_ch, levels = 0:2, ordered = TRUE, labels = c("A", "B", "C"))
 baseline$sports_classification <- factor(baseline$sports_classification, levels = 1:5, ordered = TRUE, labels = c("very low", "low", "moderate", "high", "very high"))
 baseline$caa_malignancy <- factor(baseline$caa_malignancy, levels = c(0, 1), labels = c("benign", "malign"))
 binary_vars <- names(baseline)[
@@ -200,10 +177,6 @@ fill_dataframe <- function(data, var_to_group_by, df) {
   return(df)
 }
 
-dataframe_malignancy <- fill_dataframe(baseline, "caa_malignancy", dataframe_calculations)
-dataframe_ffr0.8 <- fill_dataframe(baseline, "ffr_0.8", dataframe_calculations)
-dataframe_ffr0.81 <- fill_dataframe(baseline, "ffr_0.81", dataframe_calculations)
-
 ## ADD STATISTICAL TESTS ##########################################################################################
 diff_means_simulation <- function(data, response_var, predictor_var, reps = 5000, direction = "two sided") {
   data <- data %>% filter(!is.na(!!sym(predictor_var)))
@@ -320,4 +293,20 @@ statistics_dataframe <- function(data, df, predictor_var, reps = 5000, direction
   return(df_stats)
 }
 
-test2 <- statistics_dataframe(baseline, dataframe_malignancy, "caa_malignancy")
+# actual analysis
+vars <- c()
+for (i in seq_along(yaml$demographics$dict_rename)) {
+    vars <- c(vars, yaml$demographics$dict_rename[[i]])
+}
+data <- baseline %>% select("record_id", all_of(vars), "sports_classification", "sports_classification_ch", "caa_malignancy", "ffr_0.8", "ffr_0.81")
+dataframe_malignancy <- fill_dataframe(data, "caa_malignancy", dataframe_calculations)
+dataframe_ffr0.8 <- fill_dataframe(data, "ffr_0.8", dataframe_calculations)
+dataframe_ffr0.81 <- fill_dataframe(data, "ffr_0.81", dataframe_calculations)
+p_values_malignancy <- statistics_dataframe(data, dataframe_malignancy, "caa_malignancy")
+p_values_ffr0.8 <- statistics_dataframe(data, dataframe_ffr0.8, "ffr_0.8")
+p_values_ffr0.81 <- statistics_dataframe(data, dataframe_ffr0.81, "ffr_0.81")
+malignancy_df <- merge(dataframe_malignancy, p_values_malignancy, by = "Variable", all = TRUE)
+ffr0.8_df <- merge(dataframe_ffr0.8, p_values_ffr0.8, by = "Variable", all = TRUE)
+ffr0.81_df <- merge(dataframe_ffr0.81, p_values_ffr0.81, by = "Variable", all = TRUE)
+
+write_xlsx(malignancy_df, paste0(yaml$demographics$output_dir, "/malignancy.xlsx"))
--- statistical_analysis/log_regression.r
+++ statistical_analysis/log_regression.r
@@ -11,7 +11,8 @@ baseline <- baseline %>%
         ccta_ostial_pn = ccta_ostial_a / ccta_dist_a,
         ccta_slo_num = ccta_ostial_elliptic / ccta_ostial_pn,
         ccta_quali = ifelse(ccta_photon == "yes", 3, ifelse(is.na(ccta_quali) & ccta_photon == "no", 2, ccta_quali)),
-        ffr_0.8 = ifelse(ffr_0.8 == "yes", 1, 0)
+        ffr_0.8 = ifelse(ffr_0.8 == "yes", 1, 0),
+        ffr_0.81 = ifelse(ffr_0.81 == "yes", 1, 0)
     )
 
 # functions needed
@@ -27,7 +28,7 @@ simple_logistic_regression <- function(baseline_data, response, explanatory, fam
     buffer <- (max - min) / 50
 
     explanatory_data <- tibble(
-        !!sym(explanatory) := seq(min - buffer, max + buffer, length.out = 50)
+        !!sym(explanatory) := seq(min - 2 * buffer, max + 2 * buffer, length.out = 50)
     )
     
     prediction_data <- explanatory_data %>% 
@@ -52,8 +53,10 @@ simple_logistic_regression <- function(baseline_data, response, explanatory, fam
         geom_line() +
         geom_hline(yintercept = 1, linetype = "dashed") +
         scale_x_continuous(breaks = seq(round(min - buffer, 0), round(max + buffer), round(max / 10, 1))) +
-        scale_y_continuous(breaks = seq(0, 8, 1))
+        scale_y_continuous(breaks = seq(0, round(max(prediction_data$odds_ratio)), 1))
 
+    confusion <- NULL
+    plot_acc <- NULL
     tryCatch(
         {
             actual_response <- data[[response]]
@@ -64,8 +67,6 @@ simple_logistic_regression <- function(baseline_data, response, explanatory, fam
         },
         error = function(e) {
             print("Check model! No binary response")
-            confusion <- "No confusion matrix"
-            plot_acc <- "No accuracy plot"
         }
     )
     return(list(mdl, prediction_data, plot_log, plot_odds, confusion, plot_acc))
@@ -92,103 +93,179 @@ logistic_regression_categorical <- function(baseline_data, response, explanatory
             print("Unfactor the explanatory variable first for confusion matrix and accuracy plot!")
         }
     )
-
     return(list(mdl, odds_ratio, lower_ci, upper_ci, confusion, plot_acc))
 }
 
-# exploration of ccta_slo_num, ccta_mla_elliptic, ccta_imc_length, ccta_pn_dist, ccta_aa_degree
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_slo_num")
-mdl <- all[[1]] # significant
-summary(mdl)
-prediction_data <- all[[2]]
-print(prediction_data, n = 50)
-plot_log <- all[[3]]
-plot_log
-plot_odds <- all[[4]]
-plot_odds
-confusion <- all[[5]]
-summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
-plot_acc
+# regression models #################################################################################################
+# exploration of continuous variables: ccta_slo_num, ccta_mla_elliptic, ccta_imc_length, ccta_pn_dist, ccta_aa_degree
+# slit-like ostium combination of area and elliptic ratio with formula {slo_a / slo_elliptic}
+all_slo <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_slo_num")
+mdl_slo_ffr <- all_slo[[1]] # significant
+summary(mdl_slo_ffr)
+exp(coef(mdl_slo_ffr))
+prediction_data_slo <- all_slo[[2]]
+print(prediction_data_slo, n = 50)
+plot_log_slo <- all_slo[[3]]
+plot_log_slo 
+plot_odds_slo <- all_slo[[4]]
+plot_odds_slo
+confusion_slo <- all_slo[[5]]
+confusion_slo
+summary(confusion_slo, event_level = "second")
+plot_acc_slo <- all_slo[[6]]
+plot_acc_slo
 
 # test to split the variable again into ostial area and ostial ellipticity
 data <- baseline %>% select(ccta_ostial_a, ccta_ostial_elliptic, ffr_0.8) %>% drop_na()
-mdl <- glm(ffr_0.8 ~ ccta_ostial_a + ccta_ostial_elliptic, data = data, family = "binomial") # only ostial area is important
-mdl <- glm(ffr_0.8 ~ ccta_ostial_a * ccta_ostial_elliptic, data = data, family = "binomial") # shows that interaction doesn't matter
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_ostial_a")
-mdl <- all[[1]]
-summary(mdl) # significant
+mdl_slo_multi <- glm(ffr_0.8 ~ ccta_ostial_a + ccta_ostial_elliptic, data = data, family = "binomial") # only ostial area is important
+mdl_slo_multi_interaction <- glm(ffr_0.8 ~ ccta_ostial_a * ccta_ostial_elliptic, data = data, family = "binomial") # shows that interaction doesn't matter
+
+all_ost_a <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_ostial_a")
+mdl_ost_a_ffr <- all_ost_a[[1]]
+summary(mdl_ost_a_ffr) # significant
 prediction_data <- all[[2]]
 print(prediction_data, n = 50)
-plot_log <- all[[3]]
-plot_log
-plot_odds <- all[[4]]
-plot_odds
-confusion <- all[[5]]
-confusion
-summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
-plot_acc
+plot_log_ost_a <- all_ost_a[[3]]
+plot_log_ost_a
+plot_odds_ost_a <- all_ost_a[[4]]
+plot_odds_ost_a
+confusion_ost_a <- all_ost_a[[5]]
+confusion_ost_a
+summary(confusion_ost_a, event_level = "second")
+plot_acc_ost_a <- all_ost_a[[6]]
+plot_acc_ost_a
 
+
+# double check that elliptic is not significant
+all_ost_ellip <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_ostial_elliptic")
+mdl_ost_ellip_ffr <- all_ost_ellip[[1]]
+summary(mdl_ost_ellip_ffr)
+prediction_data_ost_ellip <- all_ost_ellip[[2]]
+print(prediction_data_ost_ellip, n = 50)
+
+
+# ostium proximal narrowing
+all_ost_pn <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_ostial_pn")
+mdl_ost_pn_ffr <- all_ost_pn[[1]]
+summary(mdl_ost_pn_ffr) # not significant
+prediction_data_ost_pn <- all_ost_pn[[2]]
+print(prediction_data_ost_pn, n = 50)
+
+
+# mla
+all_mla <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_mla_a")
+mdl_mla_ffr <- all_mla[[1]]
+summary(mdl_mla_ffr) # significant
+prediction_data_mla <- all_mla[[2]]
+print(prediction_data_mla, n = 50)
+plot_log_mla <- all_mla[[3]]
+plot_log_mla
+plot_odds_mla <- all_mla[[4]]
+plot_odds_mla
+confusion_mla <- all_mla[[5]]
+confusion_mla
+summary(confusion_mla, event_level = "second")
+plot_acc_mla <- all_mla[[6]]
+plot_acc_mla
 
 
 # exploration of ccta_mla_elliptic
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_mla_elliptic")
-mdl <- all[[1]] 
-summary(mdl) # not significant
-prediction_data <- all[[2]]
-plot_log <- all[[3]]
-plot_odds <- all[[4]]
-confusion <- all[[5]]
+all_ellip <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_mla_elliptic")
+mdl_ellip <- all_ellip[[1]] 
+summary(mdl_ellip) # not significant
+prediction_data_ellip <- all_ellip[[2]]
+print(prediction_data_ellip, n = 50)
+plot_log_ellip <- all_ellip[[3]]
+plot_odds_ellip <- all_ellip[[4]]
+plot_odds_ellip
+confusion_ellip <- all_ellip[[5]]
 summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
+plot_acc <- all_ellip[[6]]
+
+
+# exploration of ccta_pn_dist
+all_pn <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_pn_dist")
+mdl_pn_ffr <- all_pn[[1]]
+summary(mdl_pn_ffr) # not significant
+prediction_data_pn <- all_pn[[2]]
+print(prediction_data_pn, n = 50)
+plot_log_pn <- all_pn[[3]]
+plot_log_pn
+plot_odds_pn <- all_pn[[4]]
+plot_odds_pn
+confusion_pn <- all_pn[[5]]
+summary(confusion_pn, event_level = "second")
+plot_acc_pn <- all_pn[[6]]
+plot_acc_pn
 
 
 # exploration of ccta_imc_length
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_imc_length")
-mdl <- all[[1]]
-summary(mdl) # not significant
-prediction_data <- all[[2]]
-print(prediction_data, n = 50)
-plot_log <- all[[3]]
-plot_log
-plot_odds <- all[[4]]
-plot_odds
-confusion <- all[[5]]
-confusion
-summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
-plot_acc
+all_imc <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_imc_length")
+mdl_imc_ffr <- all_imc[[1]]
+summary(mdl_imc_ffr) # not significant
+prediction_data_imc <- all_imc[[2]]
+print(prediction_data_imc, n = 50)
+plot_log_imc <- all_imc[[3]]
+plot_log_imc
+plot_odds_imc <- all_imc[[4]]
+plot_odds_imc
+confusion_imc <- all_imc[[5]]
+confusion_imc
+summary(confusion_imc, event_level = "second")
+plot_acc_imc <- all_imc[[6]]
+plot_acc_imc
 
-# exploration of ccta_pn_dist
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_pn_dist")
-mdl <- all[[1]]
-summary(mdl) # not significant
-prediction_data <- all[[2]]
-print(prediction_data, n = 50)
-plot_log <- all[[3]]
-plot_log
-plot_odds <- all[[4]]
-plot_odds
-confusion <- all[[5]]
-summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
-plot_acc
 
 # exploration of ccta_aa_degree
-all <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_aa_degree")
-mdl <- all[[1]]
-summary(mdl) # not significant
-prediction_data <- all[[2]]
-print(prediction_data, n = 50)
-plot_log <- all[[3]]
-plot_log
-plot_odds <- all[[4]]
-plot_odds
-confusion <- all[[5]]
-summary(confusion, event_level = "second")
-plot_acc <- all[[6]]
-plot_acc
+all_aa <- simple_logistic_regression(baseline, "ffr_0.8", "ccta_aa_degree")
+mdl_aa_ffr <- all_aa[[1]]
+summary(mdl_aa_ffr) # not significant
+prediction_data_aa <- all_aa[[2]]
+print(prediction_data_aa, n = 50)
+plot_log_aa <- all_aa[[3]]
+plot_log_aa
+plot_odds_aa <- all_aa[[4]]
+plot_odds_aa
+confusion_aa <- all_aa[[5]]
+summary(confusion_aa, event_level = "second")
+plot_acc_aa <- all_aa[[6]]
+plot_acc_aa
+
+# forest plot
+# looping through models is pain!
+forest_df <- data.frame(
+    var = c("ostial_area", "ostial_elliptic", "ostial_pn", "mla", "mla_elliptic", "pn_dist", "imc_length", "aa_degree"),
+    odds_ratio = c(exp(mdl_ost_a_ffr$coefficients[2]), exp(mdl_ost_ellip_ffr$coefficients[2]), exp(mdl_ost_pn_ffr$coefficients[2]), exp(mdl_mla_ffr$coefficients[2]), exp(mdl_ellip$coefficients[2]), exp(mdl_pn_ffr$coefficients[2]), exp(mdl_imc_ffr$coefficients[2]), exp(mdl_aa_ffr$coefficients[2])),
+    lower_ci = c(exp(confint(mdl_ost_a_ffr))[2], exp(confint(mdl_ost_ellip_ffr))[2], exp(confint(mdl_ost_pn_ffr))[2], exp(confint(mdl_mla_ffr))[2], exp(confint(mdl_ellip))[2], exp(confint(mdl_pn_ffr))[2], exp(confint(mdl_imc_ffr))[2], exp(confint(mdl_aa_ffr))[2]),
+    upper_ci = c(exp(confint(mdl_ost_a_ffr))[4], exp(confint(mdl_ost_ellip_ffr))[4], exp(confint(mdl_ost_pn_ffr))[4], exp(confint(mdl_mla_ffr))[4], exp(confint(mdl_ellip))[4], exp(confint(mdl_pn_ffr))[4], exp(confint(mdl_imc_ffr))[4], exp(confint(mdl_aa_ffr))[4])
+)
+ggplot(data = forest_df, aes(y = var, x = odds_ratio, xmin = lower_ci, xmax = upper_ci)) +
+    geom_point() +
+    geom_errorbarh(height = 0.2) +
+    geom_vline(xintercept = 1, linetype = "dashed") +
+    theme_minimal()
+
+# quick check how much model would change with cut-off 0.81
+mdl_ost_a_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_ostial_a", family = "binomial")
+mdl_ost_ellip_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_ostial_elliptic", family = "binomial")
+mdl_ost_pn_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_ostial_pn", family = "binomial")
+mdl_mla_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_mla_a", family = "binomial")
+mdl_ellip_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_mla_elliptic", family = "binomial")
+mdl_pn_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_pn_dist", family = "binomial")
+mdl_imc_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_imc_length", family = "binomial")
+mdl_aa_ffr_081 <- simple_logistic_regression(baseline, "ffr_0.81", "ccta_aa_degree", family = "binomial")
+forest_df0.81 <- data.frame(
+    var = c("ostial_area", "ostial_elliptic", "ostial_pn", "mla", "mla_elliptic", "pn_dist", "imc_length", "aa_degree"),
+    odds_ratio = c(exp(mdl_ost_a_ffr_081[[1]]$coefficients[2]), exp(mdl_ost_ellip_ffr_081[[1]]$coefficients[2]), exp(mdl_ost_pn_ffr_081[[1]]$coefficients[2]), exp(mdl_mla_ffr_081[[1]]$coefficients[2]), exp(mdl_ellip_081[[1]]$coefficients[2]), exp(mdl_pn_ffr_081[[1]]$coefficients[2]), exp(mdl_imc_ffr_081[[1]]$coefficients[2]), exp(mdl_aa_ffr_081[[1]]$coefficients[2])),
+    lower_ci = c(exp(confint(mdl_ost_a_ffr_081[[1]]))[2], exp(confint(mdl_ost_ellip_ffr_081[[1]]))[2], exp(confint(mdl_ost_pn_ffr_081[[1]]))[2], exp(confint(mdl_mla_ffr_081[[1]]))[2], exp(confint(mdl_ellip_081[[1]]))[2], exp(confint(mdl_pn_ffr_081[[1]]))[2], exp(confint(mdl_imc_ffr_081[[1]]))[2], exp(confint(mdl_aa_ffr_081[[1]]))[2]),
+    upper_ci = c(exp(confint(mdl_ost_a_ffr_081[[1]]))[4], exp(confint(mdl_ost_ellip_ffr_081[[1]]))[4], exp(confint(mdl_ost_pn_ffr_081[[1]]))[4], exp(confint(mdl_mla_ffr_081[[1]]))[4], exp(confint(mdl_ellip_081[[1]]))[4], exp(confint(mdl_pn_ffr_081[[1]]))[4], exp(confint(mdl_imc_ffr_081[[1]]))[4], exp(confint(mdl_aa_ffr_081[[1]]))[4])
+)
+ggplot(data = forest_df0.81, aes(y = var, x = odds_ratio, xmin = lower_ci, xmax = upper_ci)) +
+    geom_point() +
+    geom_errorbarh(height = 0.2) +
+    geom_vline(xintercept = 1, linetype = "dashed") +
+    theme_minimal()
+
 
 ####################################################################################################################
 # binary explanatory variable
@@ -229,7 +306,7 @@ all <- logistic_regression_categorical(categorical, "ffr_0.8", "caa_angle")
 mdl <- all[[1]]
 summary(mdl) # not significant
 
-# forest plot
+# forest plots
 vars = c("caa_slo", "caa_pn")
 forest_df <- data.frame(var = character(), odds_ratio = numeric(), lower_ci = numeric(), upper_ci = numeric(), stringsAsFactors = FALSE)
 for (var in vars){
